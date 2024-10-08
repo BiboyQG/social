7a53991bd7a64ff3b474170580d567def6fce883
Banghao <banghao_chi@163.com>
2024-10-08 09:45:01 -0500

    fix: delete usersContextMiddleware

--- cmd/api/users.go
+++ cmd/api/users.go
@@ -37,32 +37,6 @@ func (app *application) getUserHandler(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
-func (app *application) usersContextMiddleware(next http.Handler) http.Handler {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		ctx := r.Context()
-
-		userID, err := strconv.ParseInt(chi.URLParam(r, "userID"), 10, 64)
-		if err != nil {
-			app.badRequest(w, r, err)
-			return
-		}
-
-		user, err := app.store.Users.GetByID(ctx, userID)
-		if err != nil {
-			switch {
-			case errors.Is(err, store.ErrNoRecord):
-				app.notFound(w, r, err)
-			default:
-				app.internalServerError(w, r, err)
-			}
-			return
-		}
-
-		ctx = context.WithValue(ctx, userCtxKey, user)
-		next.ServeHTTP(w, r.WithContext(ctx))
-	})
-}
-
 //	@Summary		Follow User
 //	@Description	Follow a user by user ID
 //	@Tags			Users
